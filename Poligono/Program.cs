0b120859240a551dc8d6ba4f4e7527c073974b78
vazeri <gzerox@gmail.com>
2015-06-06 01:46:29 -0500

    Subida
    Version funcional, hay que cambiar los prisma por this y eliminar lo
    static

---
+++ Poligono/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Erick;
+
+//Vazquez Gonzalez Erick 8CV11
+
+namespace Poligono
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(80, 55);
+            int i, n;
+            VolumenPrisma[] Calculos;
+
+            Erick.Libreria.Portada();
+       CIN: Console.WriteLine("\nInserte el numero de poligonos:");
+            string entry = Console.ReadLine();
+            int.TryParse(entry, out n);                 
+            if(n > 0){
+                Calculos = new VolumenPrisma[n];
+                for (i = 0; i < n; i++)                     
+                {
+                    Calculos[i] = new VolumenPrisma();      
+                    VolumenPrisma.Registro(Calculos[i]);    
+                }
+                for (i = 0; i < n; i++)                     
+                {
+                    VolumenPrisma.Imprimir(Calculos[i]);    
+                }
+            }
+            else { Console.WriteLine(" - Introdusca un valor entero valido - "); goto CIN; };
+            System.Threading.Thread.Sleep(50000);
+        }
+    }
+}
---
+++ Poligono/VolumenPrisma.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poligono
+{
+public class VolumenPrisma
+{
+    public int Apotema, TamLado, NumLados, Altura, Lado;
+    public int Perimetro, Area, Volumen;                
+
+    public VolumenPrisma(){ }
+
+    public static int cPerimetro (int NumLados, int Lado)
+    {
+        int P;
+        P=(NumLados*Lado);
+        return P;    
+    }
+
+    public static int cVolumen(int Area, int Altura)
+    {
+        int V;
+        V = Area * Altura;
+        return V;
+    }
+
+    public static int cArea(int Perimetro, int Apotema)
+    {
+        int A;
+        A = (Perimetro * Apotema)/2;
+        return A;
+    }
+
+public static void Registro(VolumenPrisma prisma)
+    {
+
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine("Indique No. de lados: ");
+    prisma.Lado = Int32.Parse(Console.ReadLine());
+
+    Console.WriteLine("Indique tamanio de apotema: ");
+    prisma.Apotema = Int32.Parse(Console.ReadLine());
+
+    Console.WriteLine("Indique tamanio de lado: ");
+    prisma.TamLado = Int32.Parse(Console.ReadLine());
+
+    Console.WriteLine("Indique la altura: ");
+    prisma.Altura = Int32.Parse(Console.ReadLine());
+
+    prisma.Perimetro = cPerimetro(prisma.TamLado, prisma.Lado);
+    prisma.Area = cArea(prisma.Perimetro, prisma.Apotema);
+    prisma.Volumen = cVolumen(prisma.Area, prisma.Altura);
+}
+
+
+public static void Imprimir(VolumenPrisma prisma) {
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine("Lados= {0} ", prisma.Lado);
+        Console.WriteLine("Tam Lado = {0} ", prisma.TamLado);
+        Console.WriteLine("Apotema = {0} ", prisma.Apotema);
+        Console.WriteLine("Perimetro = {0} ", prisma.Perimetro);
+        Console.WriteLine("Area = {0} ", prisma.Area); 
+        Console.WriteLine("Volumen = {0} ", prisma.Volumen);
+        }
+}
+}
